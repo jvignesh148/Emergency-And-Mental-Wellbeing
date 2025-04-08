e51c13fead159bb85079b446c4ab27380493c8c2
VIGNESH J <133660156+jvignesh148@users.noreply.github.com>
2025-04-08 20:02:14 +0530

    Update PreviousReports.jsx

--- Emergency_and_Mental_wellbeing/src/Components/PreviousReports.jsx
+++ Emergency_and_Mental_wellbeing/src/Components/PreviousReports.jsx
@@ -40,7 +40,7 @@ const PreviousReports = () => {
                     <li><a href='/chatbot'>Chatbot</a></li>
                     <li><a href='#videos'>videos</a></li>
                     <li><a href='/mood-track'>Mood Track</a></li>
-                    <li><a href='#new-api'>News API</a></li>
+                    <li><a href='/news-api'>News API</a></li>
                     <li><a href='/task-management'>Task Management</a></li>
                 </ul>
             </nav>
